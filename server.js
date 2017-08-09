0f1f0168b0723213ac919b8f05553f47bd094362
Kenyu Sakuraba <kenyu.sakuraba.1023@gmail.com>
2017-08-09 16:21:11 -0700

    version 1.0.1

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var POST = process.env.PORT || 8080;
 
+app.use('/', express.static(__dirname + '/public/'));
 app.get('/', function(req, res) {
-   res.sendFile(__dirname + '/index.html');
+   res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(POST, function() {
---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Foundation from 'react-foundation'
+
+const App = () => (
+	<div>
+		<h1>Yuki</h1>
+		<h1>Yuki</h1>
+	</div>
+)
+export default App;
