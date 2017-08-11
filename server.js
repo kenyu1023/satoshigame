14e5d57242a22436f4734e2988bc4f68c48dd3a9
YOS <yuki8963@gmail.com>
2017-08-11 15:45:34 -0700

    project changed

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var POST = process.env.PORT || 8080;
 
-app.use('/', express.static(__dirname + '/public/'));
+app.use('/', express.static(__dirname + '/client/public/'));
 app.get('/', function(req, res) {
-   res.sendFile(__dirname + '/public/index.html');
+   res.sendFile(__dirname + '/client/public/index.html');
 });
 
 http.listen(POST, function() {
