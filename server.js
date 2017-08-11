f5829fcf8da370b5073df60bc8dbf41649fe088a
YOS <yuki8963@gmail.com>
2017-08-11 16:23:57 -0700

    server side changed

--- server.js
+++ server.js
@@ -1,13 +1,82 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var POST = process.env.PORT || 8080;
+import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+import mongoose from 'mongoose'
 
-app.use('/', express.static(__dirname + '/client/public/'));
-app.get('/', function(req, res) {
-   res.sendFile(__dirname + '/client/public/index.html');
-});
+const app = express()
+const port = process.env.PORT || 3001
 
-http.listen(POST, function() {
-    console.log('Server running :', POST);
-});
+
+const mongodbHost = 'ds161640.mlab.com';
+const mongodbPort = '61640';
+const authenticate = 'vankyadmin:vanky1234@';
+const mongodbDatabase = 'heroku_wg2xkfhc';
+
+var url = 'mongodb://'+authenticate+mongodbHost+':'+mongodbPort + '/' + mongodbDatabase;
+console.log(url);
+
+app.use(express.static(path.join(__dirname, 'client/public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+mongoose.connect(url,{ useMongoClient: true }, dbErr => {
+  if (dbErr) throw new Error(dbErr)
+  else console.log('db connected')
+
+  app.post('/api/users', (request, response) => {
+    const { name, password } = request.body
+
+    new User({
+      name,
+      password,
+      status : 1
+    }).save(err => {
+      if (err) response.status(500)
+      else {
+        User.find({}, (findErr, userArray) => {
+          if (findErr) response.status(500).send()
+          else response.status(200).send(userArray)
+        })
+      }
+    })
+  })
+
+  app.get('/api/users', (request, response) => {
+    User.find({}, (err, userArray) => {
+      if (err) response.status(500).send()
+      else response.status(200).send(userArray)
+    })
+  })
+
+  app.put('/api/users', (request, response) => {
+    const { id } = request.body
+    console.log(id);
+    // User.findByIdAndUpdate(id, { upassword: {"age": 1} }, err => {
+    //   if (err) response.status(500).send()
+    //   else {
+    //     User.find({}, (findErr, userArray) => {
+    //       if (findErr) response.status(500).send()
+    //       else response.status(200).send(userArray)
+    //     })
+    //   }
+    // })
+  })
+
+  app.delete('/api/users', (request, response) => {
+    const { id } = request.body
+    User.findByIdAndRemove(id, err => {
+      if (err) response.status(500).send()
+      else {
+        User.find({}, (findErr, userArray) => {
+          if (findErr) response.status(500).send()
+          else response.status(200).send(userArray)
+        })
+      }
+    })
+  })
+
+  app.listen(port, err => {
+    if (err) throw new Error(err)
+    else console.log(`listening on port ${port}`)
+  })
+})
