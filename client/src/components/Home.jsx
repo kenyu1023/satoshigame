12389fc22ba1c7129dbd8d1142aef4045b4e6154
Kenyu Sakuraba <kenyu.sakuraba.1023@gmail.com>
2017-08-11 17:21:58 -0700

    home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,9 +12,29 @@ export default class App extends React.Component {
 		render(){
 			return (
 				<div>
-					<div className="page-top">
-						<h1>Hi Yuki</h1>
-					</div>
+						<div className="page-top">
+							<img className="logo" src="https://upload.wikimedia.org/wikipedia/en/2/20/Los_Angeles_Dodgers_Logo.png" />
+							<h1>Hi AHO</h1>
+						</div>
+
+						<div className="intro">
+							<div className="intro-para">
+								<h2>Hi This is an intro</h2>
+								<p>intro text here I am Satoshi Takasawa fake paragaraph here this is going to be about three breakpoint para</p>
+							</div>
+						</div>
+
+						<div className="about">
+							<div className="about-para">
+								<h3>I am a Designer</h3>
+								<p>I am a Japanese artist who specialise in game character design, game environmetn design and CG.
+									 I have five years of professional experience in e CG design industry.
+								</p>
+
+								<img className="portlate" src="" alt="My photo"/>
+							</div>
+						</div>
+
 				</div>
 			)
 		}
