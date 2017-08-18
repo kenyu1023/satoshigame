70ebd4a7e1c5d76ec305c51f178c630a44761078
Kenyu Sakuraba <kenyu.sakuraba.1023@gmail.com>
2017-08-18 03:01:22 -0700

    added video to home and worked on portfolio

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -14,8 +14,11 @@ export default class App extends React.Component {
 			return (
 				<div>
 						<div className="page-top">
-							<img className="logo" src="https://upload.wikimedia.org/wikipedia/en/2/20/Los_Angeles_Dodgers_Logo.png" />
-							<h1>Hi AHO</h1>
+							<video autoPlay loop id="video-background" muted>
+  						<source src="https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761" type="video/mp4" />
+							</video>
+							{/* <img className="logo" src="https://upload.wikimedia.org/wikipedia/en/2/20/Los_Angeles_Dodgers_Logo.png" /> */}
+							<h1 className="name">SATOSHI TAKAZAWA</h1>
 						</div>
 
 						<div className="intro">
@@ -83,7 +86,6 @@ export default class App extends React.Component {
 
 						<div className="contact">
 							<div className="myInfo">
-								<h2>This is my Info</h2>
 								<h1>Satoshi Takasawa</h1>
 								<p>mail.address@mail.com</p>
 								<div className="social">
--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -8,6 +8,13 @@ export default class App extends React.Component {
 			this.state = {
 
 			};
+
+
+
+			// function ModalImgChange(){
+			// 	var modalImg = document.getElementById("ModalMainImg");
+
+			// }
 		}
 		render(){
 			return (
@@ -19,6 +26,52 @@ export default class App extends React.Component {
 
 					<div className="portfolio-main">
 						<div className="works-panel">
+							<a href="#test"><img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" /></a>
+						</div>
+
+						<div className="modal" id="test">
+							<div className="modal-container">
+								<div className="modal-img">
+									<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" id="ModalMainImg"alt="#" />
+								</div>
+								<div className="modal-sub-img-container">
+								<div className="modal-sub-img">
+									<img src="https://www.downgraf.com/wp-content/uploads/2015/09/Video-Game-Character-Design-Collection-002.jpg" alt="#" id="ModalSubImg1" onClick="ModalImgChange()"/>
+								</div>
+
+									<div className="modal-sub-img">
+									<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+								</div>
+
+									<div className="modal-sub-img">
+									<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+								</div>
+
+									<div className="modal-sub-img">
+									<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+								</div>
+
+									<div className="modal-sub-img">
+									<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+								</div>
+								</div>
+								<div className="modal-para">
+									<h1>Title lorem ipsum</h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<a href="#modal-close">close</a>
+								</div>
+							</div>
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
 							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
 						</div>
 
@@ -34,6 +87,56 @@ export default class App extends React.Component {
 							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
 						</div>
 
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
+
+						<div className="works-panel">
+							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
+						</div>
 						<div className="works-panel">
 							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
 						</div>
@@ -46,7 +149,7 @@ export default class App extends React.Component {
 							<img src="https://i.pinimg.com/736x/d7/61/38/d76138de84a31689daaa36be8df56fbd--game-character-design-main-character.jpg" alt="#" />
 						</div>
 					</div>
-				</div>
+					</div>
 			)
 		}
 }
